e6049ccc42cf517a477d595ebf5e2fbdcc00cbeb
tylerkeyes <tyler.keyes00@gmail.com>
2024-06-09 19:04:40 -0400

    update function names

--- assert.go
+++ assert.go
@@ -2,13 +2,13 @@ package assert
 
 import "log"
 
-func assert(err error) {
+func Assert(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
-func assert_msg(err error, msg string) {
+func AssertMsg(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%v: %v\n", msg, err)
 	}
